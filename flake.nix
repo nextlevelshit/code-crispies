7ab095718bbfac3f38d1d7b6bf5188458bcbdabc
Michael Czechowski <mail@dailysh.it>
2026-01-25 21:28:16 +0100

    chore(nix): add flake with claude-code in devShell

--- flake.nix
+++ flake.nix
@@ -9,13 +9,14 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs_20
             nodePackages.npm
             gnumake
+            claude-code
           ];
 
           shellHook = ''
